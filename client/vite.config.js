ddb01e56e6f6a50163de8609d8aff8001ef9ca33
ahmadmujtaba2121 <your-github-email@example.com>
2024-12-26 21:44:34 +0500

    Update: Install Firebase dependencies
    Update: Install Firebase dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,52 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage', 'firebase/database'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
     commonjsOptions: {
-      esmExternals: true
-    }
+      include: [/node_modules/],
+      extensions: ['.js', '.jsx'],
+      strictRequires: true,
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-hotkeys-hook'
+          ],
+          firebase: [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/firestore',
+            'firebase/storage',
+            'firebase/database'
+          ]
+        },
+      },
+    },
   },
   optimizeDeps: {
-    include: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage', 'firebase/database']
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/firestore',
+      'firebase/storage',
+      'firebase/database',
+      'react-hotkeys-hook'
+    ]
   }
 });
