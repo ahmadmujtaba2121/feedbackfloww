4dc7766e49557239237eeedde548e788a0497d6b
ahmadmujtaba2121 <learntocode2121@gmail.com>
2024-12-28 01:16:25 +0500

    90327523523754

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     host: true
@@ -29,5 +30,9 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
+  },
+  preview: {
+    port: 3000,
+    host: true
   }
 });
