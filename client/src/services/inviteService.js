da5fa47c7bfb2c723d34c04ed390f60277433468
ahmadmujtaba2121 <learntocode2121@gmail.com>
2024-12-27 23:56:10 +0500

    5.0

--- client/src/services/inviteService.js
+++ client/src/services/inviteService.js
@@ -1,4 +1,4 @@
-import { collection, doc, setDoc, serverTimestamp, getDoc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { collection, doc, setDoc, serverTimestamp, getDoc, updateDoc, arrayUnion, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
 import { v4 as uuidv4 } from 'uuid';
 
